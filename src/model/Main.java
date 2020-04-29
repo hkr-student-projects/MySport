bee4f63131acae50a09363f450aef39b9b6b2657
Deniel Alekseev <daniel.neo.eu@icloud.com>
2020-04-29 12:38:00 +0200

    revert commit

---
+++ src/model/Main.java
@@ -0,0 +1,17 @@
+package model;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/calendar.fxml"));
+        stage.setScene(new Scene(loader.load()));
+        stage.show();
+    }
+}
