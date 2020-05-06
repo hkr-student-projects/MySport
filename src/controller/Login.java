7c257604a028cab0962a3f6c2152cb91527d5436
Peter-Daniel-hkr <petrus.daniel@protonmail.com>
2020-05-06 22:33:49 +0200

    Updated fxml files

--- src/controller/Login.java
+++ src/controller/Login.java
@@ -1,28 +1,50 @@
 package controller;
 
 import com.jfoenix.controls.JFXToggleButton;
+import com.restfb.DefaultFacebookClient;
+import com.restfb.FacebookClient;
+import com.restfb.Parameter;
+import com.restfb.Version;
+import com.restfb.exception.FacebookException;
+import com.restfb.scope.ScopeBuilder;
+import com.restfb.types.User;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.paint.Paint;
 import javafx.scene.shape.Line;
+import javafx.scene.text.Text;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
+import javafx.stage.Stage;
 import model.App;
 import model.Database.DatabaseManager;
 import model.Logging.Logger;
 import model.Tools.SceneSwitcher;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ResourceBundle;
 
+import static com.restfb.logging.RestFBLogger.CLIENT_LOGGER;
+
 public class Login extends Menu implements Initializable {
 
     @FXML
     private TextField email;
+    @FXML
+    private Button forgotPassword;
     @FXML
     private PasswordField password;
     @FXML
@@ -33,6 +55,17 @@ public class Login extends Menu implements Initializable {
     private JFXToggleButton toggle;
     @FXML
     private Line line0, line1;
+
+    private String appId = "2864933576924620";
+    private String appSecret = "5b11cb850b5100f411224c64f188360f";
+    private Stage stage;
+
+    private static final String SUCCESS_URL = "https://www.facebook.com/connect/login_success.html";
+
+
+
+
+
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -119,4 +152,110 @@ public class Login extends Menu implements Initializable {
     protected void onAppClose() {
 
     }
+    public void handleFBLoginButton(ActionEvent event) {
+        System.out.println("In handle button");
+        WebView webView = new WebView();
+        WebEngine webEngine = webView.getEngine();
+        System.out.println("In startLogin");
+        /*
+        // create the scene
+        stage.setTitle("Facebook Login Example");
+        // use quite a wide window to handle cookies popup nicely
+        stage.setScene(new Scene(new VBox(webView), 1000, 600, Color.web("#666970")));
+        stage.show();
+        */
+
+        // obtain Facebook access token by loading login page
+        stage = new Stage();
+        DefaultFacebookClient facebookClient = new DefaultFacebookClient(Version.LATEST);
+        String loginDialogUrl = facebookClient.getLoginDialogUrl(appId, SUCCESS_URL, new ScopeBuilder());
+        System.out.println(loginDialogUrl);
+        webEngine.load(loginDialogUrl + "&display=popup&response_type=code");
+        webEngine.locationProperty().addListener((property, oldValue, newValue) -> {
+                    if (newValue.startsWith(SUCCESS_URL)) {
+                        // extract access token
+                        CLIENT_LOGGER.debug(newValue);
+                        int codeOffset = newValue.indexOf("code=");
+                        String code = newValue.substring(codeOffset + "code=".length());
+                        FacebookClient.AccessToken accessToken = facebookClient.obtainUserAccessToken(
+                                appId, appSecret, SUCCESS_URL, code);
+
+                        // trigger further code's execution
+                        consumeAccessToken(accessToken);
+
+                        // close the app as goal was reached
+                        stage.close();
+                        try {
+                            goToScreen2(accessToken.getAccessToken());
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+
+                    } else if ("https://www.facebook.com/dialog/close".equals(newValue)) {
+                        throw new IllegalStateException("dialog closed");
+                    }
+                }
+        );
+
+        Scene scene = new Scene(webView);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void goToScreen2(String accessToken) throws Exception{
+        FacebookClient client = new DefaultFacebookClient(accessToken, Version.LATEST);
+        User user = null;
+        try
+        {
+            user = client.fetchObject("me", User.class, Parameter.with("fields", "name,email"));
+
+        }
+        catch (FacebookException ex)
+        {
+        }
+        if(stage != null) {
+            URL url = new File("src/view/Account.fxml").toURI().toURL();
+            Parent root = FXMLLoader.load(url);
+            Text lblData = (Text) root.lookup("#welcomeLabel");
+            if(user != null && lblData != null) {
+                lblData.setText("Welcome " + user.getName());
+            }
+
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        }
+    }
+
+
+    private static void consumeAccessToken(FacebookClient.AccessToken accessToken) {
+        CLIENT_LOGGER.debug("Access token: " + accessToken.getAccessToken());
+        CLIENT_LOGGER.debug("Expires: " + accessToken.getExpires());
+    }
+
+    public void handleForgotPasswordAction(ActionEvent actionEvent) {
+        //show a screen to receive the user's email address
+        if(stage != null) {
+            URL url = null;
+            try {
+                url = new File("src/view/ResetCode.fxml").toURI().toURL();
+                Parent root = FXMLLoader.load(url);
+                Scene scene = new Scene(root);
+                stage.setScene(scene);
+                stage.show();
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+
+
 }
+
+
+
+
--- src/controller/Settings.java
+++ src/controller/Settings.java
@@ -1,14 +1,28 @@
 package controller;
 
+import com.jfoenix.controls.JFXComboBox;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
 import java.net.URL;
+import java.util.Locale;
 import java.util.ResourceBundle;
 
 public class Settings extends Menu implements Initializable {
+    @FXML
+    private JFXComboBox<String> comboBox;
+    private ResourceBundle resourceBundle;
+    private Locale locale;
+    ObservableList list = FXCollections.observableArrayList("English", "Swedish", "Norwegian");
+
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        comboBox = new JFXComboBox<>();
+        comboBox.setItems(list);
+
         bindTab(this);
     }
 
@@ -30,5 +44,9 @@ public class Settings extends Menu implements Initializable {
     @Override
     protected void onAppClose() {
 
+    }
+    private void loadLangBundle(String lang){
+        locale = new Locale(lang);
+        resourceBundle = ResourceBundle.getBundle("src/langBundle", locale);
     }
 }
