b6e2832745b1ca53f2050ff214d893b014ad6f70
Deniel Alekseev <daniel.neo.eu@icloud.com>
2020-05-06 10:20:18 +0200

    Changs in module-info

--- src/module-info.java
+++ src/module-info.java
@@ -6,14 +6,14 @@ module MySport {
     requires java.sql;
     requires mysql.connector.java;
     requires kotlin.stdlib;
+    requires javafx.base;
 
-
-    opens model;
     opens model.Logging;
     opens model.Localization;
     opens model.Database;
     opens model.People;
     opens model.Tools;
+    opens model;
     opens view;
     opens view.css;
     opens view.img;
