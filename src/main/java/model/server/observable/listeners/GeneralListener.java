48745bf298a6dfce0c6e4fd581cd105214e33f02
Peter-Daniel-hkr <petrus.daniel@protonmail.com>
2020-05-22 19:46:47 +0200

    Added package "listeners" to server

---
+++ src/main/java/model/server/observable/listeners/GeneralListener.java
@@ -0,0 +1,9 @@
+package model.server.observable.listeners;
+
+
+import observable.events.ObserverEvent;
+
+public interface GeneralListener<T> {
+    void propertyChange(ObserverEvent<T> var1) throws Exception;
+}
+
