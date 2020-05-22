0fc79deb78791941f890aaaf16236940087a17eb
Peter-Daniel-hkr <petrus.daniel@protonmail.com>
2020-05-22 20:13:46 +0200

    Added "viewModel" to client

---
+++ src/main/java/model/client/viewModel/ConversationRowData.java
@@ -0,0 +1,49 @@
+package model.client.viewModel;
+
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+public class ConversationRowData {
+
+    private int conversationID;
+    private StringProperty name;
+    private StringProperty mostRecentMessage;
+
+    public ConversationRowData(int conversationID, String name, String mostRecentMessage) {
+        this.conversationID = conversationID;
+        this.name = new SimpleStringProperty(name);
+        this.mostRecentMessage = new SimpleStringProperty(mostRecentMessage);
+    }
+
+    public int getConversationID() {
+        return conversationID;
+    }
+
+    public void setConversationID(int conversationID) {
+        this.conversationID = conversationID;
+    }
+
+    public String getName() {
+        return name.get();
+    }
+
+    public StringProperty nameProperty() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name.set(name);
+    }
+
+    public String getMostRecentMessage() {
+        return mostRecentMessage.get();
+    }
+
+    public StringProperty mostRecentMessageProperty() {
+        return mostRecentMessage;
+    }
+
+    public void setMostRecentMessage(String mostRecentMessage) {
+        this.mostRecentMessage.set(mostRecentMessage);
+    }
+}
---
+++ src/main/java/model/client/viewModel/UserRowData.java
@@ -0,0 +1,36 @@
+package model.client.viewModel;
+
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import model.client.models.User;
+
+
+public class UserRowData {
+    private StringProperty username;
+    private User user;
+
+    public UserRowData(User user) {
+        this.username = new SimpleStringProperty(user.getName());
+        this.user = user;
+    }
+
+    public String getUsername() {
+        return username.get();
+    }
+
+    public StringProperty usernameProperty() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username.set(username);
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
