a32ef2f6829f83f112f1f086901fe7fbd9e8c446
Peter-Daniel-hkr <petrus.daniel@protonmail.com>
2020-05-25 01:10:13 +0200

    Added Password Unmasking

---
+++ src/main/java/model/PasswordUnmasking/PeekablePasswordFieldSkin.java
@@ -0,0 +1,97 @@
+package model.PasswordUnmasking;
+
+
+import javafx.event.EventHandler;
+import javafx.scene.Cursor;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.control.skin.TextFieldSkin;
+import javafx.scene.input.MouseEvent;
+
+
+class PeekablePasswordFieldSkin extends TextFieldSkin {
+
+    private boolean _doMask = true;
+
+    public PeekablePasswordFieldSkin(TextField textField) {
+
+        super(textField);
+
+        if (textField instanceof PasswordField) {
+            textField.getStylesheets().add(PeekablePasswordFieldSkin.class.getResource("peekablepasswordfield.css").toExternalForm());
+            textField.getStyleClass().addAll("withicon", "showing");
+
+            textField.setOnMousePressed(new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent event) {
+                    double vIconsLeftMargin = textField.getWidth() - 20;
+                    final boolean vDoMask = event.getX() <= vIconsLeftMargin;
+                    setDoMask(vDoMask);
+                    setIcon(vDoMask);
+                }
+            });
+
+            textField.setOnMouseReleased(new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent event) {
+                    if (!_doMask) {
+                        setDoMask(true);
+                        setIcon(true);
+                    }
+                }
+            });
+
+            textField.setOnMouseMoved((MouseEvent event) -> {
+                double vIconsLeftMargin = textField.getWidth() - 20;
+                if (event.getX() > vIconsLeftMargin) {
+                    textField.setCursor(Cursor.HAND);
+                } else {
+                    textField.setCursor(Cursor.TEXT);
+                }
+            });
+
+            textField.setOnMouseExited((MouseEvent event) -> {
+                setDoMask(true);
+                setIcon(true);
+            });
+        }
+    }
+
+    /**
+     * Allows switching from "masking" to "unmasking" mode
+     *
+     * @param pValue if true text must be masked
+     */
+    private void setDoMask(boolean pValue) {
+
+        _doMask = pValue;
+
+        TextField textField = this.getSkinnable();
+
+        String vText = textField.getText();
+        textField.setText(vText);
+
+        setIcon(pValue);
+    }
+
+
+    private void setIcon(boolean pShowing) {
+        TextField textField = this.getSkinnable();
+        textField.getStyleClass().removeAll("showing", "hiding");
+        if (pShowing) {
+            textField.getStyleClass().add("showing");
+        } else {
+            textField.getStyleClass().add("hiding");
+        }
+    }
+
+    @Override
+    protected String maskText(String txt) {
+        if (_doMask) {
+            return super.maskText(txt);
+        } else {
+            return txt;
+        }
+    }
+
+}
