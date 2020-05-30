72d9faeb7cd0182a9156dfde9ebb1e76d1422936
kristijan00 <57765162+kristijan00@users.noreply.github.com>
2020-05-30 19:05:35 +0200

    CreateAcc updated

--- src/main/java/controller/CreateAccount.java
+++ src/main/java/controller/CreateAccount.java
@@ -42,42 +42,37 @@ public class CreateAccount implements Initializable {
         addFocusStyle(new TextField[]{firstname, middlename, surname, ssn, mobile, email, password, repassword},
                 line0, line1, line2, line3, line4, line5, line6, line7
         );
-//        email.setOnKeyTyped(e -> {
-//            emailsList = App.mySqlManager.getEmail();
-//            if (!emailsList.isEmpty()) {
-//                if (email.getText().matches("^[A-Za-z0-9+_.-]+@([A-Za-z0-9]{2,10}\\.)+[A-Za-z]{2,8}$")) {
-//                    ThreadResult<String, Boolean> emailCheck = new ThreadResult<>(this::getEmails, email.getText());
-//                    Thread thread = new Thread(emailCheck);
-//                    thread.start();
-//                    try {
-//                        thread.join();
-//                    } catch (InterruptedException interruptedException) {
-//                        interruptedException.printStackTrace();
-//                    }
-//                    if (emailCheck.getValue()) {
-//                        emailFormat = false;
-//                        redLine(line5);
-//                        emailError.setVisible(true);
-//                    } else
-//                        emailFormat = true;
-//                } else {
-//                    emailFormat = false;
-//                    resetLine(line5);
-//                    emailError.setVisible(false);
-//                }
-//
-//                if (e.getCode() == KeyCode.BACK_SPACE && email.getText().isEmpty())
-//                    emailStr = "";
-//                else if (e.getCode() == KeyCode.BACK_SPACE && email.getText().length() > 0)
-//                    emailStr = emailStr.substring(0, emailStr.length() - 2);
-//                else
-//                    emailStr += e.getText();
-//            } else {
-//                emailFormat = false;
-//                redLine(line5);
-//                emailError.setVisible(true);
-//            }
-//        });
+        emailsList = App.mySqlManager.getEmail();
+        email.setOnKeyTyped(e -> {
+                if (email.getText().matches("^[A-Za-z0-9+_.-]+@([A-Za-z0-9]{2,10}\\.)+[A-Za-z]{2,8}$")) {
+                    ThreadResult<String, Boolean> emailCheck = new ThreadResult<>(this::getEmails, email.getText());
+                    Thread thread = new Thread(emailCheck);
+                    thread.start();
+                    try {
+                        thread.join();
+                    } catch (InterruptedException interruptedException) {
+                        interruptedException.printStackTrace();
+                    }
+                    if (emailCheck.getValue()) {
+                        emailFormat = false;
+                        redLine(line5);
+                        emailError.setVisible(true);
+                    } else
+                        emailFormat = true;
+                } else {
+                    emailFormat = false;
+                    resetLine(line5);
+                    emailError.setVisible(false);
+                }
+
+                if (e.getCode() == KeyCode.BACK_SPACE && email.getText().isEmpty())
+                    emailStr = "";
+                else if (e.getCode() == KeyCode.BACK_SPACE && email.getText().length() > 0)
+                    emailStr = emailStr.substring(0, emailStr.length() - 2);
+                else
+                    emailStr += e.getText();
+
+        });
     }
 
     private void addFocusStyle(TextField[] fields, Line... lines) {
@@ -100,16 +95,16 @@ public class CreateAccount implements Initializable {
 //        return App.mySqlManager.existsEmail(email);
 //    }
 
-//    private Boolean getEmails(String email) {
-//        ArrayList<String> entries = emailsList.get("email");
-//
-//        for (int i = 0; i < entries.size(); i++) {
-//            if (entries.get(i).matches(email)) {
-//                return true;
-//            }
-//        }
-//        return false;
-//    }
+    private Boolean getEmails(String email) {
+        ArrayList<String> entries = emailsList.get("email");
+
+        for (int i = 0; i < entries.size(); i++) {
+            if (entries.get(i).matches(email)) {
+                return true;
+            }
+        }
+        return false;
+    }
 
     private void resetLine(Line line) {
         line.setStroke(Paint.valueOf("#000000"));
--- src/main/java/model/App.java
+++ src/main/java/model/App.java
@@ -43,7 +43,7 @@ public class App extends Application {
 //        System.setErr(new PrintStream("/dev/null"));// hide warnings
         System.setErr(new PrintStream("NUL:"));// for windows
         primaryStage.getIcons().add(new Image("view/img/jarIcon.png"));
-        primaryStage.setScene(SceneSwitcher.instance.getScene("LoginRich"));
+        primaryStage.setScene(SceneSwitcher.instance.getScene("Login"));
         primaryStage.initStyle(StageStyle.UTILITY);
         primaryStage.setResizable(false);
         primaryStage.show();
